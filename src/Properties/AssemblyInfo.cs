48f27835746dda3654954abf5d29fce503bbfcba
RHcomunications <108822770+RHcomunications@users.noreply.github.com>
2026-08-12 20:04:22 -0600

    release: v2026.08.11.4 - AudioMap y MusicMap canonicos, audio.pac con claves planas y cero cadenas crudas en las llamadas de audio

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Bejeweled 3 Accesible")]
-[assembly: AssemblyDescription("Edición accesible de Bejeweled 3 con lectores de pantalla y soporte total por teclado")]
+[assembly: AssemblyDescription("EdiciÃ³n accesible de Bejeweled 3 con lectores de pantalla y soporte total por teclado")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Narayan Projects")]
 [assembly: AssemblyProduct("Bejeweled 3 Accesible")]
-[assembly: AssemblyCopyright("Copyright © Narayan Projects 2026")]
+[assembly: AssemblyCopyright("Copyright Â© Narayan Projects 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("8f3214a1-561b-4d2a-8954-2195f1906a21")]
-[assembly: AssemblyVersion("2026.08.11.3")]
+[assembly: AssemblyVersion("2026.08.11.4")]
 
-[assembly: AssemblyFileVersion("2026.08.11.3")]
+[assembly: AssemblyFileVersion("2026.08.11.4")]
