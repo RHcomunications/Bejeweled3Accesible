1251cada265f30b8da670bb6e89de3401458a7de
RHcomunications <108822770+RHcomunications@users.noreply.github.com>
2026-08-26 15:03:29 -0600

    Android: version android-v2026.08.26.5 (fix TalkBack en apaisado) - bumps csproj/AutoUpdater/Localization/README/context

--- src/Android/Update/AndroidAutoUpdater.cs
+++ src/Android/Update/AndroidAutoUpdater.cs
@@ -15,7 +15,7 @@ namespace Bejeweled3Accessible.AndroidApp.Update
     public static class AndroidAutoUpdater
     {
         public const string GitHubRepo = "RHcomunications/Bejeweled3Accesible";
-        public const string CurrentVersion = "2026.08.26.4";
+        public const string CurrentVersion = "2026.08.26.5";
 
         public class AndroidReleaseInfo
         {
