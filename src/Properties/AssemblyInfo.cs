3d21b9aee77622ed67f4bc7173d9786b14322ad4
RHcomunications <108822770+RHcomunications@users.noreply.github.com>
2026-09-07 14:24:03 -0600

    chore(release): bump unified version to v2026.09.07.1 across Windows and Android

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -13,6 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("8f3214a1-561b-4d2a-8954-2195f1906a21")]
-[assembly: AssemblyVersion("2026.09.01.1")]
-
-[assembly: AssemblyFileVersion("2026.09.01.1")]
+[assembly: AssemblyVersion("2026.09.07.1")]
+[assembly: AssemblyFileVersion("2026.09.07.1")]
